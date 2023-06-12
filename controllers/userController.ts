352aeab433040cdda5920c18804066b8b2ed014a
lwood54 <lwood3499@gmail.com>
2023-06-12 08:02:50 -0500

    rm unused import

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import prisma from "../db/db.prisma";
